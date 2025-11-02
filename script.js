2ffa72193d6f53a157a177b3134132c5402bd170
anjalikamat23 <anjalikamat32@gmail.com>
2025-11-02 12:10:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! I will get back to you soon.');
+    // In a real application, you would send the form data to a server here.
+});
